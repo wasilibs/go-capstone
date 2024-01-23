30f18991889f9fd16e0cbbfb710c8d66103434ba
Takeshi Yoneda <t.y.mathetake@gmail.com>
2024-01-22 16:16:28 -0800

    review
    Signed-off-by: Takeshi Yoneda <t.y.mathetake@gmail.com>

--- cmd/decodeasm/main.go
+++ cmd/decodeasm/main.go
@@ -54,6 +54,10 @@ func main() {
 	cp := capstone.NewCapstone(arch, mode)
 	defer cp.Close()
 	res := cp.Decode(opcode)
+	if len(res) == 0 {
+		fmt.Fprintf(flag.CommandLine.Output(), "expected at least 1 instruction, got %d\n", len(res))
+		os.Exit(1)
+	}
 	for _, d := range res {
 		fmt.Println(d)
 	}
